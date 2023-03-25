c17758dc46fd7618d76a77a4cb94f4e32c472d4c
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-03-25 20:51:22 +0900

    feat: CRUD-C 코드

--- session/blog/urls.py
+++ session/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/<int:blog_id>',views.detail,name='detail'),
+    path('blog/new',views.new,name='new'),
+    path('blog/create',views.create,name='create'),
 ]
--- session/blog/views.py
+++ session/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Blog
 
 def home(request):
@@ -8,4 +8,13 @@ def home(request):
 def detail(request,blog_id):
     blog = get_object_or_404(Blog,pk=blog_id)
     return render(request,'detail.html',{'blog':blog})
+
+def new(request):
+    return render(request,'new.html')
     
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.content = request.POST['content']
+    new_blog.save()
+    return redirect('detail',new_blog.id)
